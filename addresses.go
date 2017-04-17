bd367969a70d1ed596d8695be50c0569a0afab1e
Ronny Bangsund <github@neuralshortcircuit.com>
2017-04-17 22:42:20 +0200

    Initial testing of address formatting.

---
+++ addresses.go
@@ -0,0 +1,18 @@
+package countries
+
+import (
+	"fmt"
+)
+
+// FormatAddress tries to make sense of the typical input fields given for addresses,
+// and format them into a proper address the way it's usually shown in the country.
+func FormatAddress(line1, line2, zip, city, state, cc string) string {
+	var a string
+	switch cc {
+	case "NO", "SE":
+		a = fmt.Sprintf("%s\n%s\n%s %s\n%s", line1, line2, zip, city, Country(cc))
+	default:
+		a = fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", line1, line2, zip, city, state, Country(cc))
+	}
+	return a
+}
